7e2c716e60e21348cdbea32560398ebfb67d20bd
unknown <sam_1700@hotmail.com>
2015-03-09 15:37:51 +0100

    Better js api test

--- addons/b2/b2.js
+++ addons/b2/b2.js
@@ -1,11 +1,10 @@
 var b2 = require('./../../node_modules/bindings')('b2');
 
+var test_inp = '0011';
 
-var str_result = b2.b10("0011");
+var str_result = b2.b10(test_inp);
 console.log('str_result:', str_result);
 
 
-var Uint8_result = b2.b10(
-  new Uint8Array( '0011'.split('') )
-);
+var Uint8_result = b2.b10( new Uint8Array(test_inp.split('')) );
 console.log('Uint8_result', Uint8_result);
