dbdf6b3e7f7739f8902f8a62dfb24c4b7bfe671c
unknown <sam_1700@hotmail.com>
2015-03-10 14:21:36 +0100

    Adapting new raw API in tests

--- test/mocha/ascii.js
+++ test/mocha/ascii.js
@@ -6,20 +6,16 @@ var base = require('./../../index.js'),
 
 describe('ascii', function (){
 
-  describe('to', function (){
-
-    describe('base64', function (){
-      it('returns {string}cGxlYXN1cmUu when arg1={string}pleasure.', function (){
-        assert.equal(ascii.to[64]('pleasure.'), 'cGxlYXN1cmUu');
-      });
-      it('returns {string}bGVhc3VyZS4= when arg1={string}leasure.', function (){
-        assert.equal(ascii.to[64]('leasure.'), 'bGVhc3VyZS4=');
-      });
-      it('returns {string}ZWFzdXJlLg== when arg1={string}easure.', function (){
-        assert.equal(ascii.to[64]('easure.'), 'ZWFzdXJlLg==');
-      });
+  describe('base64', function (){
+    it('returns {string}cGxlYXN1cmUu when arg1={string}pleasure.', function (){
+      assert.equal(ascii('pleasure.'), 'cGxlYXN1cmUu');
     });
-
+    it('returns {string}bGVhc3VyZS4= when arg1={string}leasure.', function (){
+      assert.equal(ascii('leasure.'), 'bGVhc3VyZS4=');
+    });
+    it('returns {string}ZWFzdXJlLg== when arg1={string}easure.', function (){
+      assert.equal(ascii('easure.'), 'ZWFzdXJlLg==');
+    });
   });
 
 });
--- test/mocha/base64.js
+++ test/mocha/base64.js
@@ -6,20 +6,16 @@ var base = require('./../../index.js'),
 
 describe('base64', function (){
 
-  describe('to', function (){
-
-    describe('ascii', function (){
-      it('returns {string}pleasure. when arg1={string}cGxlYXN1cmUu', function (){
-        assert.equal(base64.to['ascii']('cGxlYXN1cmUu'), 'pleasure.');
-      });
-      it('returns {string}leasure. when arg1={string}bGVhc3VyZS4=', function (){
-        assert.equal(base64.to['ascii']('bGVhc3VyZS4='), 'leasure.');
-      });
-      it('returns {string}easure. when arg1={string}ZWFzdXJlLg==', function (){
-        assert.equal(base64.to['ascii']('ZWFzdXJlLg=='), 'easure.');
-      });
+  describe('ascii', function (){
+    it('returns {string}pleasure. when arg1={string}cGxlYXN1cmUu', function (){
+      assert.equal(base64['ascii']('cGxlYXN1cmUu'), 'pleasure.');
     });
-
+    it('returns {string}leasure. when arg1={string}bGVhc3VyZS4=', function (){
+      assert.equal(base64['ascii']('bGVhc3VyZS4='), 'leasure.');
+    });
+    it('returns {string}easure. when arg1={string}ZWFzdXJlLg==', function (){
+      assert.equal(base64['ascii']('ZWFzdXJlLg=='), 'easure.');
+    });
   });
 
 });
